a3a715d93bd0b12f1615b452482f11255a1eef52
Leneyxis <syedumerahmed97@gmail.com>
2024-12-14 03:38:00 +0530

    test

--- scripts/long-form.js
+++ scripts/long-form.js
@@ -587,8 +587,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         profileForm.classList.add("summary-active");
 
         let html = `
-            <h1>Profile Summary</h1>
-            <p>Review your information below. Click 'Edit information' to make changes. You can now return to the plugin to start autofilling jobs</p>
+            <p1>Review your information below. Click 'Edit information' to make changes. You can now return to the plugin to start autofilling jobs</p1>
             <h2>Current Information</h2>
             <button class="edit-information">Edit information</button>
             <h3>Personal Information</h3>
